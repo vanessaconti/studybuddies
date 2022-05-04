f65ffcfa6559473265e8b7958bfded1fabe1a768
vanessaconti <vconti901@aol.com>
2022-05-04 03:01:16 -0400

    updated table, fixed error

--- db/seed.sql
+++ db/seed.sql
@@ -14,7 +14,7 @@ INSERT INTO groups (study_group_id, name, main_focus, date_formed, contact_email
 (8, 'Five Boro Devs', 'Database Fundamentals', 'September 30 2022','fiveborodevs@fakeemail.com'),
 (9, 'PERN Stack Lovers', 'PERN Stack for Beginners', 'October 01 2022','pernlovers@email.com');
 
-INSERT INTO events (eventid, name, virtual_meeting_link, start_time, end_time, number_of_attendees, study_group_id, cancelled) VALUES
-(1, 'All you need to know', 'https://www.zoom.us/fakelink1','10:00 AM', '2:00 PM', 20, 1, false),
-(2, 'Javascript Codealongs', 'https://www.zoom.us/fakelink2','11:00 AM', '4:00 PM', 20, 2, false),
-(3, 'MVP And Where To Begin', 'https://www.zoom.us/fakelink3', '9:00 AM', '3:00 PM', 35, 3, false);
+INSERT INTO events (eventid, name, virtual_meeting_link, start_time, end_time, number_of_attendees, study_group_id) VALUES
+(1, 'All you need to know', 'https://www.zoom.us/fakelink1','10:00 AM', '2:00 PM', 20, 1),
+(2, 'Javascript Codealongs', 'https://www.zoom.us/fakelink2','11:00 AM', '4:00 PM', 20, 2),
+(3, 'MVP And Where To Begin', 'https://www.zoom.us/fakelink3', '9:00 AM', '3:00 PM', 35, 3);
